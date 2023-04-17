6068b1544fb7ec84912921ec8da06ee2b0f2686c
Sohithvishnusai <sohithvishnusai@air.com>
2023-04-17 19:23:38 +0200

    updated scikit learn version commit

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from model.model import __version__
 model_version = __version__
 app = FastAPI()
 
+
 class TextIn(BaseModel):
     text: str
+
 
 class PredictionOut(BaseModel):
     language: str
 
 
-@app.get("/")
+@app.get("/predict")
 async def root():
     return {"health_check": "ok", "model_version": model_version}
 
-@app.post("/predict", response_model=PredictionOut)
+
+@app.post("/", response_model=PredictionOut)
 def predict(payload: TextIn):
     language = predict_pipeline(payload.text)[0]
-    return{"language": language}
+    return {"language": language}
